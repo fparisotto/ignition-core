637b80d9995e646b5260605c049a76487a054d03
Allan Oliveira <allan@chaordicsystems.com>
2015-10-27 19:57:00 -0200

    Many improvements

--- src/main/scala/ignition/core/jobs/utils/SparkContextUtils.scala
+++ src/main/scala/ignition/core/jobs/utils/SparkContextUtils.scala
@@ -13,6 +13,7 @@ import ignition.core.utils.DateUtils._
 
 import scala.collection.JavaConversions._
 import scala.collection.mutable.ArrayBuffer
+import scala.collection.mutable
 import scala.io.{Codec, Source}
 import scala.reflect.ClassTag
 import scala.util.Try
@@ -69,12 +70,12 @@ object SparkContextUtils {
       processPaths((p) => sc.textFile(p), paths, minimumPaths)
     }
 
-    private def processParallelTextFiles(paths: Seq[String], minimumPaths: Int, maxBytesPerPartition: Long): RDD[String] = {
+    private def processParallelTextFiles(paths: Seq[String], minimumPaths: Int, maxBytesPerPartition: Long, minPartitions: Int): RDD[String] = {
       val splittedPaths = paths.flatMap(ignition.core.utils.HadoopUtils.getPathStrings)
       if (splittedPaths.size < minimumPaths)
         throw new Exception(s"Not enough paths found for $paths")
 
-      parallelTextFiles(splittedPaths, maxBytesPerPartition)
+      parallelTextFiles(splittedPaths, maxBytesPerPartition, minPartitions)
     }
 
     private def filterPaths(paths: Seq[String],
@@ -146,6 +147,7 @@ object SparkContextUtils {
     }
 
 
+    @deprecated("It may incur heavy S3 costs and/or be slow with small files, use getParallelTextFiles instead", "2015-10-27")
     def getTextFiles(paths: Seq[String], synchLocally: Boolean = false, forceSynch: Boolean = false, minimumPaths: Int = 1): RDD[String] = {
       if (synchLocally)
         processTextFiles(synchToHdfs(paths, processTextFiles, forceSynch), minimumPaths)
@@ -153,14 +155,17 @@ object SparkContextUtils {
         processTextFiles(paths, minimumPaths)
     }
 
-    def getParallelTextFiles(paths: Seq[String], maxBytesPerPartition: Long, synchLocally: Boolean = false, forceSynch: Boolean = false, minimumPaths: Int = 1): RDD[String] = {
+    def getParallelTextFiles(paths: Seq[String],
+                             maxBytesPerPartition: Long = 64 * 1000 * 1000,
+                             minPartitions: Int = 500,
+                             synchLocally: Boolean = false, forceSynch: Boolean = false, minimumPaths: Int = 1): RDD[String] = {
       if (synchLocally)
-        processParallelTextFiles(synchToHdfs(paths, processTextFiles, forceSynch), minimumPaths, maxBytesPerPartition)
+        processParallelTextFiles(synchToHdfs(paths, processTextFiles, forceSynch), minimumPaths, maxBytesPerPartition, minPartitions)
       else
-        processParallelTextFiles(paths, minimumPaths, maxBytesPerPartition)
+        processParallelTextFiles(paths, minimumPaths, maxBytesPerPartition, minPartitions)
     }
 
-    @deprecated("It may incur in heavy S3 costs and/or be slow with small files, use filterAndGetParallelTextFiles instead", "2015-10-27")
+    @deprecated("It may incur heavy S3 costs and/or be slow with small files, use filterAndGetParallelTextFiles instead", "2015-10-27")
     def filterAndGetTextFiles(path: String,
                               requireSuccess: Boolean = false,
                               inclusiveStartDate: Boolean = true,
@@ -180,6 +185,7 @@ object SparkContextUtils {
 
     def filterAndGetParallelTextFiles(path: String,
                                       maxBytesPerPartition: Long = 64 * 1000 * 1000,
+                                      minPartitions: Int = 500,
                                       requireSuccess: Boolean = false,
                                       inclusiveStartDate: Boolean = true,
                                       startDate: Option[DateTime] = None,
@@ -193,7 +199,7 @@ object SparkContextUtils {
       val paths = getFilteredPaths(Seq(path), requireSuccess, inclusiveStartDate, startDate, inclusiveEndDate, endDate, lastN, ignoreMalformedDates)
       if (paths.size < minimumPaths)
         throw new Exception(s"Tried with start/end time equals to $startDate/$endDate for path $path but but the resulting number of paths $paths is less than the required")
-      getParallelTextFiles(paths, maxBytesPerPartition, synchLocally, forceSynch, minimumPaths)
+      getParallelTextFiles(paths, maxBytesPerPartition, minPartitions, synchLocally, forceSynch, minimumPaths)
     }
 
     private def stringHadoopFile(paths: Seq[String], minimumPaths: Int): RDD[Try[String]] = {
@@ -237,17 +243,18 @@ object SparkContextUtils {
         objectHadoopFile(paths, minimumPaths)
     }
 
-    def parallelTextFiles(paths: Seq[String], maxBytesPerPartition: Long): RDD[String] = {
+    def parallelTextFiles(paths: Seq[String], maxBytesPerPartition: Long, minPartitions: Int): RDD[String] = {
+      require(paths.nonEmpty, "At least one path is required")
       val hadoopConf = sc.broadcast(sc.hadoopConfiguration.iterator().map { case entry => entry.getKey -> entry.getValue }.toMap)
 
       val foundFiles = parallelListFiles(paths)
-      val partitionedFiles = sc.parallelize(foundFiles.map(_.path)).map(file => file -> ()).partitionBy(createPartitioner(foundFiles, maxBytesPerPartition))
+      val partitionedFiles = sc.parallelize(foundFiles.map(_.path)).map(file => file -> ()).partitionBy(createPartitioner(foundFiles, maxBytesPerPartition, minPartitions))
 
       partitionedFiles.mapPartitions { files =>
         val conf = hadoopConf.value.foldLeft(new Configuration()) { case (acc, (k, v)) => acc.set(k, v); acc }
         val codecFactory = new CompressionCodecFactory(conf)
+        val fileSystem = FileSystem.get(new java.net.URI(paths.head), conf)
         files.map { case (path, _) => path } flatMap { path =>
-          val fileSystem = FileSystem.get(new java.net.URI(path), conf)
           val hadoopPath = new Path(path)
           val inputStream = Option(codecFactory.getCodec(hadoopPath)) match {
             case Some(compression) => compression.createInputStream(fileSystem.open(hadoopPath))
@@ -267,16 +274,22 @@ object SparkContextUtils {
       }
     }
 
-    private def createPartitioner(files: Seq[HadoopFile], maxBytesPerPartition: Long): Partitioner = {
-      val partitions = files.foldLeft(Seq.empty[HadoopFilePartition]) {
+    private def createPartitioner(files: Seq[HadoopFile], maxBytesPerPartition: Long, minPartitions: Long): Partitioner = {
+      implicit val ordering: Ordering[HadoopFilePartition] = Ordering.by(p => -p.size) // Small partitions come first (highest priority)
+
+      val pq: mutable.PriorityQueue[HadoopFilePartition] = mutable.PriorityQueue.empty
+
+      (0L until minPartitions).foreach(_ => pq += HadoopFilePartition(0, Seq.empty))
+
+      val partitions = files.foldLeft(pq) {
         case (acc, file) =>
-          acc.find(bucket => bucket.size + file.size < maxBytesPerPartition) match {
+          acc.headOption.filter(bucket => bucket.size + file.size < maxBytesPerPartition) match {
             case Some(found) =>
               val updated = found.copy(size = found.size + file.size, paths = file.path +: found.paths)
-              acc.updated(acc.indexOf(found), updated)
-            case None => acc :+ HadoopFilePartition(file.size, Seq(file.path))
+              acc.tail += updated
+            case None => acc += HadoopFilePartition(file.size, Seq(file.path))
           }
-      }
+      }.filter(_.size > 0).toList // Remove empty partitions
 
       val indexedPartitions: Map[Any, Int] = partitions.zipWithIndex.flatMap {
         case (bucket, index) => bucket.paths.map(path => path -> index)
