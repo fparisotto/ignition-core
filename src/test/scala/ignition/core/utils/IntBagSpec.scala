603dae78b8b4c633bfce46655f19e34249d32810
Allan Douglas R. de Oliveira <allan@chaordicsystems.com>
2016-06-14 19:52:08 -0300

    Added percentile do IntBag

--- src/test/scala/ignition/core/utils/IntBagSpec.scala
+++ src/test/scala/ignition/core/utils/IntBagSpec.scala
@@ -10,14 +10,24 @@ class IntBagSpec extends FlatSpec with ShouldMatchers  {
     IntBag.from(Seq(1, 1, 2, 2, 2, 3, 4, 4, 4, 4, 4)).histogram shouldBe Map(1 -> 2, 2 -> 3, 3 -> 1, 4 -> 5)
   }
 
-  it should "calculate the median and average" in {
+  it should "calculate the average" in {
     val size = 1000
-    val numbers = (0 until 1000).map(_ => Random.nextInt(400).toLong).toList
+    val numbers = (0 until size).map(_ => Random.nextInt(400).toLong).toList
     val bag = IntBag.from(numbers)
 
     bag.avg.get shouldBe numbers.sum / size
+  }
 
-    // TODO: the median is only approximate and it could be better, improve it
+  it should "calculate the percentile, min and max" in {
+    val size = 3 // anything different is hard to guess because of the approximation
+    val numbers = (0 until size).map(_ => Random.nextInt(400).toLong).toList
+    val bag = IntBag.from(numbers)
+
+    bag.min.get shouldBe numbers.min
+    bag.percentile(0.1).get shouldBe numbers.min
+    bag.median.get shouldBe numbers.sorted.apply(1)
+    bag.percentile(99.9).get shouldBe numbers.max
+    bag.max.get shouldBe numbers.max
   }
 
 }
